64871acefafa4ce446323007fd09a848d983f408
Evgenii <evg4755@yandex.rugit config --global user.name evgeniieglobal user.email evg4755@yandex.ru>
2022-09-28 10:31:55 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int len = num.Length;
+if (len == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"{num} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{num} - Нe палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {num} - не является пятизначным");
+}
---
+++ задание 23/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = ReadInt("Введите число N: ");
+for (int N = 1; N <= number; N++)
+{ 
+    Console.Write($"{N*N*N} ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
